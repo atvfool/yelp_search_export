c0c194c881a51b8458776d9c353f5a3b26174321
ahayden_shoptech <ahayden@shoptech.com>
2020-01-23 15:24:12 -0500

    requires a api.key file. saves results to places.csv. created to find places around work

---
+++ yelp.py
@@ -0,0 +1,72 @@
+import time
+import os
+import os.path
+import requests
+import json
+
+from os import path
+
+f = open("api.key", "r")
+APIKEY = f.read().rstrip()
+
+def fetch_results(lat, lng, rad, cat, limit, term):
+    url = "https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={lng}&radius={rad}&categories={cat}&limit={limit}&term={term}".format(lat=lat,lng=lng, rad=rad,cat=cat,limit=limit, term=term)
+    headers = {"Authorization" : APIKEY}
+
+    response = requests.get(url, headers=headers)
+    res = json.loads(response.text)
+
+    return res["businesses"]
+
+startingLat = 39.2857099
+startingLng = -84.364996
+rad = 15000
+# Converts meters to miles then multiplies by 69.172 to get the degrees (24901.92 miles cirumfrence of Equator / 360 degrees = 69.172 miles per degree)
+varienceInDegrees = (rad / 1609.34) / 69.172
+minLat = startingLat - varienceInDegrees
+minLng = startingLng - varienceInDegrees
+maxLat = startingLat + varienceInDegrees
+maxLng = startingLng + varienceInDegrees
+cat = "restaurants"
+limit = 50
+term = ""
+
+filename = "places.csv"
+places = list()
+
+print("Getting results for {cat} with search term {term}".format(cat=cat, term=term))
+businesses = list()
+""" This is to get a wider net and longer list, it searchs in a 3x3 grid pattern
+    -x,+y   x,+y    +x,+y
+    -x,y    x,y     +x,y
+    -x,-y   x,-y    +x,-y
+    x,y are the starting latitude and longitude
+"""
+businesses.append(fetch_results(startingLat, startingLng, rad, cat, limit, term))
+businesses.append(fetch_results(minLat, startingLng, rad, cat, limit, term))
+businesses.append(fetch_results(maxLat, startingLng, rad, cat, limit, term))
+businesses.append(fetch_results(startingLat, minLng, rad, cat, limit, term))
+businesses.append(fetch_results(startingLat, maxLng, rad, cat, limit, term))
+businesses.append(fetch_results(minLat, minLng, rad, cat, limit, term))
+businesses.append(fetch_results(minLat, maxLng, rad, cat, limit, term))
+businesses.append(fetch_results(maxLat, minLng, rad, cat, limit, term))
+businesses.append(fetch_results(maxLat, maxLng, rad, cat, limit, term))
+
+for business in businesses:
+    for result in business:
+        if not any(x["name"] == result["name"] for x in places):
+            places.append(result)
+
+
+print("{count} places found.".format(count=len(places)))
+print("Deleting old file")
+if path.exists(filename):
+    os.remove(filename)
+
+print("Writing to file")
+with open(filename, "w") as filehandle:
+    for place in places:
+        filehandle.write("\"" + str(place["name"]) + "\",\"" + str(place["url"]) + "\",\"" + str(place["distance"]) + "\",\"" + str(place["id"]) + "\"\n")
+
+
+print("------------- Completed -------------")
