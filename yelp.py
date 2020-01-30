e8303ebe54c2bf6b106bc3d652f59badb728fed3
ahayden_shoptech <ahayden@shoptech.com>
2020-01-30 09:31:09 -0500

    Added some inputs so you don't have to change the file everytime you run, and added in pgeocode to get latitude and longitude based on zip code

--- yelp.py
+++ yelp.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import requests
 import json
+import pgeocode
 
 from os import path
 
@@ -18,9 +19,18 @@ def fetch_results(lat, lng, rad, cat, limit, term):
 
     return res["businesses"]
 
-startingLat = 39.2857099
-startingLng = -84.364996
-rad = 15000
+# Ask for some stuffs
+# for reference, 15000 meters is about 9.5 miles
+countryCode = input("2 character country code:")
+zipCode = input("Postal Code:")
+term = input("Search Term:")
+rad = int(input("Radius in meters:"))
+
+nomi = pgeocode.Nominatim(countryCode)
+selectedArea = nomi.query_postal_code("45248")
+startingLat = selectedArea["latitude"]
+startingLng = selectedArea["longitude"]
+
 # Converts meters to miles then multiplies by 69.172 to get the degrees (24901.92 miles cirumfrence of Equator / 360 degrees = 69.172 miles per degree)
 varienceInDegrees = (rad / 1609.34) / 69.172
 minLat = startingLat - varienceInDegrees
@@ -29,7 +39,7 @@ maxLat = startingLat + varienceInDegrees
 maxLng = startingLng + varienceInDegrees
 cat = "restaurants"
 limit = 50
-term = ""
+
 
 filename = "places.csv"
 places = list()
